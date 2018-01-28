b995b84251033be03529277970702ba39a245e84
Joapfel <kraemer_johannes@gmx.de>
2018-01-28 15:46:23 +0100

    added a possible file reading abstraction

--- src/com/ws1718/ismla/server/GreetingServiceImpl.java
+++ src/com/ws1718/ismla/server/GreetingServiceImpl.java
@@ -1,6 +1,16 @@
 package com.ws1718.ismla.server;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
+
+import javax.servlet.ServletContext;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import com.ws1718.ismla.client.GreetingService;
@@ -18,14 +28,18 @@ public class GreetingServiceImpl extends RemoteServiceServlet implements Greetin
 
 	public String greetServer(String input) throws IllegalArgumentException {
 		
+		
+		List<String> listofLines = getLinesFromFile("/transcription/Arabic_chat.txt");
+		for(String s : listofLines){
+			System.out.println(s);
+		}
+		
 		LanguageTransliterator lt = new LanguageTransliterator();
 		
 		String result = "";
 		String arabicPhon = lt.transcribeArabicToPhonetic(input);
 		String russiaRepresentation = lt.transcribeToRussian(input);
 		
-//		
-//
 //		result += arabicPhon + "\n";
 //		result += russiaRepresentation + "\n";
 		
@@ -43,5 +57,53 @@ public class GreetingServiceImpl extends RemoteServiceServlet implements Greetin
 	}
 	
 	
+	/**
+	 * 
+	 * creates the input stream to read files from the resource directory
+	 * @param path
+	 * @return
+	 */
+	public InputStream getInputStream(String path){
+		return getServletContext().getResourceAsStream("/WEB-INF/resources/" + path);
+	}
+	
+	/**
+	 * creates a buffered reader for reading a file from the resource directory
+	 * @param path
+	 * @return
+	 */
+	public BufferedReader getBufferedReader(String path){
+		return new BufferedReader(new InputStreamReader(getInputStream(path)));
+	}
+	
+	/**
+	 * creates a list of lines for a file from the resource directory
+	 * @param path
+	 * @return
+	 */
+	public List<String> getLinesFromFile(String path){
+		List<String> rval = new ArrayList<String>();
+		
+		BufferedReader reader = getBufferedReader(path);
+		String line;
+		try {
+			while ((line = reader.readLine()) != null) {
+				if(line.isEmpty()) continue;
+				rval.add(line);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return rval;
+	}
 	
 }
+
+
+
+
+
+
+
